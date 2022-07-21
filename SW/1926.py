eb8183dda9c1038af81fcbe32b016119771a9a8b
yangu1455 <kunja266@hanmail.net>
2022-07-21 16:58:45 +0900

    1926. 간단한 369게임

--- SW/1926.py
+++ SW/1926.py
@@ -1,38 +1,65 @@
 # 1926. 간단한 369게임
 
 N = int(input())
-res = []
-num = []
 
 for n in range(1, N+1):
-    num.append(n)
+    s = str(n)
 
-for x in range(1, N+1) :
+    cnt = s.count('3') + s.count('6') + s.count('9')
 
-    for i in num:
-
-        for j in range(len(str(i))):  
-
-            if '3' in str(i) or '6' in str(i) or '9' in str(i):
-                res.append('-')
-                # i -= i[j]
-            else:
-                res.append(i)
-
-print(*res)
+    if cnt == 0:
+        print(s, end = ' ')
+    else:
+        print("-"*cnt, end = ' ')
 
 
 
 
+# N = int(input())
+# res = []
+# num = []
+
+# for n in range(1, N+1):
+#     num.append(n)
+
+# for x in range(1, N+1) :
+
+#     for i in num:
+
+#         for j in range(len(str(i))):  
+
+#             if '3' in str(i) or '6' in str(i) or '9' in str(i):
+#                 res.append('-')
+#                 # i -= i[j]
+#             else:
+#                 res.append(i)
+
+# print(*res)
 
 
-# s = ''
 
-# for x in range(1, N+1):
-#     s = s + ' ' + str(x)
+# num = int(input())
+# for i in range(1,num+1):
+#     i = str(i)
+#     clap = i.count('3')+i.count('6')+i.count('9')
+#     if clap == 0:
+#         print(i, end= " ")
+#     else:
+#         print("-"*clap, end= " ")
 
-# s = s.replace('3', '-')
-# s = s.replace('6', '-')
-# s = s.replace('9', '-')
 
-# res = s.split()
+
+# n=int(input()) # 100
+ 
+# for i in range(1,n+1):
+#     cnt = 0
+#     s = str(i)
+#     for j in s:
+         
+#         if j == '3' or j == '6' or j == '9':
+#             cnt += 1       
+             
+#     if cnt == 0:
+#         print(i,end=' ')
+#     else:
+#         print("-"*cnt, end=' ',sep='')
