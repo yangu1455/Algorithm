0b464b65901570f4721d664770aea2b9a7599a2f
yangu1455 <kunja266@hanmail.net>
2022-07-27 10:11:56 +0900

    17094번 Serious Problem

---
+++ Baekjoon/17094.py
@@ -0,0 +1,20 @@
+
+# 인덱스 에러
+
+l = int(input())
+s = list(input())
+a = 0
+b = 0
+
+for _ in range(0, l):
+    if s[_] == '2':
+        a += 1
+    elif s[_] == 'e':
+        b += 1
+
+if a > b:
+    print(2)
+elif b > a:
+    print('e')
+else:
+    print('yee')
