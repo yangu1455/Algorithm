5e666f9ef90590085adc849fd59a23e0c21af731
yangu1455 <kunja266@hanmail.net>
2022-08-02 17:35:39 +0900

    5063번 TGN

---
+++ Baekjoon/5063.py
@@ -0,0 +1,12 @@
+# 5063번 TGN
+
+N = int(input())
+
+for _ in range(N):
+    r, e, c = map(int, input().split())
+    if r > e - c:
+        print("do not advertise")
+    elif r == e - c:
+        print("does not matter")
+    else:
+        print("advertise")
