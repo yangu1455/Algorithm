44ed8d2305ae7964ecab2135d70f7e2e5ee8afe5
yangu1455 <kunja266@hanmail.net>
2022-08-02 09:17:04 +0900

    2161번 카드1

---
+++ Baekjoon/2161.py
@@ -0,0 +1,25 @@
+# 2161번 카드1
+
+from collections import deque
+
+n = int(input())
+queue = deque(range(1, n+1))
+
+while len(queue) > 1:
+    print(queue.popleft(), end=" ")
+    queue.append(queue.popleft())
+
+print(queue[0])
+
+
+# ===========================================
+
+
+n = int(input())
+queue = list(range(1, n+1))
+
+while len(queue) > 1:
+    print(queue.pop(0), end = " ")
+    queue.append(queue.pop(0))
+
+print(queue[0])
