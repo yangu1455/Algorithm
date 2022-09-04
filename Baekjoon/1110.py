625b429fb470bb7f951cc43f45d18b7d37050c89
yangu1455 <kunja266@hanmail.net>
2022-09-04 18:52:41 +0900

    1110번 더하기 사이클

---
+++ Baekjoon/1110.py
@@ -0,0 +1,16 @@
+# 1110번 더하기 사이클
+
+n = num = int(input())
+count = 0
+
+while True:
+    ten = n // 10
+    one = n % 10
+    total = ten + one
+    count += 1
+    n = int(str(n % 10) + str(total % 10))
+    
+    if(num == n):
+        break
+
+print(count)
