b8d0c88df7b51bbeaf04b7fe27392a0912e315f3
yangu1455 <kunja266@hanmail.net>
2022-09-05 13:45:18 +0900

    2753번 윤년

---
+++ Baekjoon/2753.py
@@ -0,0 +1,13 @@
+# 2753번 윤년
+
+a= int(input())
+if a % 4 != 0:
+    print("0")
+else:
+    if a % 100 == 0:
+        if a % 400 == 0:
+            print("1")
+        else:
+            print("0")
+    else:
+        print("1")
