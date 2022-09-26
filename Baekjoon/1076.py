f25f022f11f150a50c703a5136d282c3a6f628f7
yangu1455 <kunja266@hanmail.net>
2022-09-27 08:59:35 +0900

    1076번 저항

--- Baekjoon/1076.py
+++ Baekjoon/1076.py
@@ -12,28 +12,45 @@
 # grey	8	100,000,000
 # white	9	1,000,000,000
 
-import sys
+
+# import sys
 
 color_dict = {
-    0: 'black', 1: 'brown', 2: 'red', 3: 'orange', 4: 'yellow', 5: 'green', 6: 'blue', 7: 'violet', 8: 'grey', 9: 'white'
-    }
+    0: "black",
+    1: "brown",
+    2: "red",
+    3: "orange",
+    4: "yellow",
+    5: "green",
+    6: "blue",
+    7: "violet",
+    8: "grey",
+    9: "white",
+}
 
 multi_list = [1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000]
 
+# sys.stdin.readline()으로 바꾸니까 틀렸다고 나왔음! 주의!
+# 이유 뭔지 알아가지고 오세용
 one = input()
 two = input()
 multiply = input()
 
+
 def dict():
+    one_n = 0
+    two_n = 0
+    multi_ = 0
 
     for i in range(len(color_dict)):
         if color_dict[i] == one:
-            one = i
-            continue
-        elif color_dict[i] == two:
-            two = i
-            continue
-        elif color_dict[i] == multiply:
-            multiply = multi_list[i]
+            one_n = i
+        if color_dict[i] == two:
+            two_n = i
+        if color_dict[i] == multiply:
+            multi_ = multi_list[i]
 
-print(((one * 10) + two) * multiply)
+    return ((one_n * 10) + two_n) * multi_
+
+
+print(dict())
