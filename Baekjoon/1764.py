081d2421c1b835b37f7de3e1308bb1ad0bd6cbaa
yangu1455 <kunja266@hanmail.net>
2022-10-05 01:33:14 +0900

    1764번 듣보잡

--- Baekjoon/1764.py
+++ Baekjoon/1764.py
@@ -1,22 +1,16 @@
 # 1764번 듣보잡
 
 # 문제
-# 김진영이 듣도 못한 사람의 명단과, 보도 못한 사람의 명단이 주어질 때, 
+# 김진영이 듣도 못한 사람의 명단과, 보도 못한 사람의 명단이 주어질 때,
 # 듣도 보도 못한 사람의 명단을 구하는 프로그램을 작성하시오.
-
-
 # 듣도 못한 사람의 명단에는 중복되는 이름이 없으며, 보도 못한 사람의 명단도 마찬가지이다.
 
-# 출력
-# 듣보잡의 수와 그 명단을 사전순으로 출력한다.
 
-
-
-# 첫째 줄에 듣도 못한 사람의 수 N, 보도 못한 사람의 수 M이 주어진다. 
+# 첫째 줄에 듣도 못한 사람의 수 N, 보도 못한 사람의 수 M이 주어진다.
 N, M = map(int, input().split())
 
-# 이어서 둘째 줄부터 N개의 줄에 걸쳐 듣도 못한 사람의 이름과, 
-# N+2째 줄부터 보도 못한 사람의 이름이 순서대로 주어진다. 
+# 이어서 둘째 줄부터 N개의 줄에 걸쳐 듣도 못한 사람의 이름과,
+# N+2째 줄부터 보도 못한 사람의 이름이 순서대로 주어진다.
 dict_ = {}
 
 for _ in range(N):
@@ -33,23 +27,18 @@ for _ in range(M):
     else:
         dict_[name_s] += 1
 
-for i in range(len(dict_)):
-    if dict_[i]
+cnt = 0
+res_ = []
 
+for k, v in dict_.items():
+    if v == 2:
+        cnt += 1
+        res_.append(k)
 
-# 입력
+print(cnt)
+res_.sort()
 
-# 3 4
-# ohhenrie
-# charlie
-# baesangwook
-# obama
-# baesangwook
-# ohhenrie
-# clinton
-
-# 출력
-
-# 2
-# baesangwook
-# ohhenrie
+# 듣보잡의 수와 그 명단을 사전순으로 출력한다.
+while res_:
+    print(res_[0])
+    res_.remove(res_[0])
