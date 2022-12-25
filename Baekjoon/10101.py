592965357ead9510053231e2f01ac7492c8287d1
yangu1455 <kunja266@hanmail.net>
2022-12-25 21:07:17 +0900

    10101번 삼각형 외우기

---
+++ Baekjoon/10101.py
@@ -0,0 +1,14 @@
+# 10101번 삼각형 외우기
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a == 60 and b == 60 and c == 60:
+    print('Equilateral')
+elif a + b + c == 180 and (a == b or a == c or b == c):
+    print('Isosceles')
+elif a + b + c == 180 and (a != b != c):
+    print('Scalene')
+elif a + b + c != 180:
+    print('Error')
