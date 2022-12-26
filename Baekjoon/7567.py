07bf27829a9b6cbe71f97a702ca52e7f86e97afe
yangu1455 <kunja266@hanmail.net>
2022-12-27 00:08:47 +0900

    7567번 그릇

---
+++ Baekjoon/7567.py
@@ -0,0 +1,14 @@
+# 7567번 그릇
+
+dish = list(str(input()))
+answer = 0
+
+for i in range(len(dish)):
+    if i == 0:
+        answer += 10
+    elif dish[i] == dish[i-1]:
+        answer += 5
+    else:
+        answer += 10
+        
+print(answer)
