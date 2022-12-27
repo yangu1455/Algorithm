5f169d06a52be7ea1b3a8c2d840b865d89a623c2
yangu1455 <kunja266@hanmail.net>
2022-12-27 16:30:22 +0900

    11286번 절댓값 힙

--- Baekjoon/11286.py
+++ Baekjoon/11286.py
@@ -34,3 +34,52 @@ for _ in range(int(sys.stdin.readline())):
         else:
             # [1] 이유!
             print(heapq.heappop(heap)[1])
+
+
+
+#==============================
+# 우선순위 큐를 이용해보자 1
+
+import heapq as hq
+import sys
+
+input = sys.stdin.readline
+pq = []
+
+for _ in range(int(input())):
+    x = int (input())
+    if x :
+        hq.heappush(pq, (abs(x), x))
+    else:
+        print(hq.heappop(pq)[1] if pq else 0)
+
+#===============================
+# 우선순위 큐를 이용해보자 2
+
+import heapq as hq
+import sys
+
+input = sys.stdin.readline
+min_heap = [] # 양수
+max_heap = [] # 음수
+for _ in range(int(input())):
+    x = int(input())
+    if x :
+        if x > 0:
+            hq.heappush(min_heap, x)
+        else:
+            hq.heappush(max_heap, -x) # 부호를 바꿔서 최대힙
+    else:
+        if min_heap:
+            if max_heap:
+                if min_heap[0] < abs(-max_heap[0]):
+                    print(hq.heappop(min_heap))
+                else:
+                    print(-hq.heappop(max_heap))
+            else:
+                print(hq.heappop(min_heap))
+        else:
+            if max_heap:
+                print(-hq.heappop(max_heap))
+            else:
+                print(0)
